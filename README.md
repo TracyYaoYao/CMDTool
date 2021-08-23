2d0c85b53c46d4f86bc3bde609e5bcaf513fd110
TracyYaoYao <18314464415@163.com>
2021-08-23 15:44:35 +0800

    fix readme.md and handle exception

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# CMDTool
+# 终端输入使用LittleServer功能
+
+### 1 接口说明
+
+LittleServer目前支持 `tinyurl` , `pastebin` , `filesave`功能
+
+```python
+# 1）tinyURL:/api/tinyurl/encode
+# 请求body
+data = {'surl': surl}
+headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+
+
+# 2）pastebin:/api/pastebin/submit
+# 请求body
+data = {'poster': POSTER, 'syntax': SYNTAX, 'content': CONTENT}
+headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+
+
+# 3）filesave:/api/filesave/put
+# 请求body
+f = open(FILE, 'rb')
+data = {'files': {'file': f}, 'key': {'key': PASSKEY}}
+```
+
+
+
+###2 使用说明
+
+切换到项目所在根目录，然后用命令行输入
+
+```python
+# 三种功能的输入字符是不区分大小的
+# 比如：tinyurl , TinyURL都可以调用tinyurl的功能
+
+python main.py tinyurl TEST_URL 
+
+python main.py pastebin POSTER SYNTAX CONTENT
+
+python main.py filesave FILE PASSKEY
+
+# eg:
+(venv) ➜  CMDTool git:(main) ✗ python main.py tinyuRL https://binacs.cn
+https://kiki.zone/r/6VniAr
+
+```
+
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import requests
 import sys, getopt
 
 data = dict()
-url_svc = 'https://kiki.zone/tinyurl'
 svc_name = ['tinyurl', 'pastebin', 'filesave']
-svc_map = {'tinyurl': 'https://kiki.zone/api/tinyurl/encode',
+svc_api = {'tinyurl': 'https://kiki.zone/api/tinyurl/encode',
            'pastebin': 'https://kiki.zone/api/pastebin/submit',
            'filesave': 'https://kiki.zone/api/filesave/put'}
 
@@ -13,43 +12,47 @@ svc_test_map = {'tinyurl': 'http://127.0.0.1:8000/api/tinyurl/encode',
                 'pastebin': 'http://127.0.0.1:8000/api/pastebin/submit',
                 'filesave': 'http://127.0.0.1:8000/api/filesave/put'}
 
-# tinyURL:/api/tinyurl/encode
-# pastebin:/api/pastebin/submit
-# filesave:/api/filesave/put
-
-
-
-
 def go_request(svcType, data, type):
     requests.packages.urllib3.disable_warnings()
-    if type == 0:
+    if not type:
         r = requests.post(svcType, data=data, headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
                       verify=False)
     else:
-        print(data['key'])
-        r = requests.post(svcType, files=data['files'], data = data['key'],
-                          # headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-                          verify=False)
+        r = requests.post(svcType, files=data['files'], data = data['key'],verify=False)  # 文件类型的header格式默认即可
     print(r.text)
 
 
 def main():
     opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="])  # 用sys.argv[1:]过滤掉第一个参数（它是执行脚本的名字，不应算作参数的一部分）
     svc = args[0].lower()
+    type = 0
     if svc == svc_name[0]:
-        svc_url = svc_map[svc_name[0]]
+        if len(args) != 2:
+            print(' Error input! Re-enter Please.')
+            print(" only SURL is required in tinyurl-api. Re-enter Please")
+            return
+        svc_url = svc_api[svc_name[0]]
         data = {'surl': args[1]}
-        type = 0
-
     elif svc == svc_name[1]:
-        svc_url = svc_map[svc_name[1]]
+        if len(args) != 4:
+            print(' Error input! Re-enter Please.')
+            print(" POSTER, SYNTAX and CONTENT are required in pastebin-api.")
+            return
+        svc_url = svc_api[svc_name[1]]
         data = {'poster': args[1], 'syntax': args[2], 'content': args[3]}
-        type = 0
-    else:
-        svc_url = svc_map[svc_name[2]]
+    elif svc == svc_name[2]:
+        if len(args) != 3:
+            print(' Error input! Re-enter Please.')
+            print(" FILE and PASSKEY are required in filesave-api. Re-enter Please")
+            return
+        type = 1
+        svc_url = svc_api[svc_name[2]]
         f = open(args[1], 'rb')
         data = {'files': {'file': f}, 'key': {'key': args[2]}}
-        type = 1
+    else:
+        print(' Error SVC-API! Re-enter Please.')
+        print(" TinyURL, PasteBin and FileSave are only supported By TracyYaoYao's LittleServer until now.")
+        return
     go_request(svc_url, data, type)
 
 if __name__ == '__main__':
